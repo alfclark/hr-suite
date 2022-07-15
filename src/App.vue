40747087107fbc95f0c3a1dd948a66def57ea9f1
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-15 16:26:01 -0700

    -Commit inicial/workbench listo/assets logo cambiados/bootstrap

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+:root {
+}
+#app {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
