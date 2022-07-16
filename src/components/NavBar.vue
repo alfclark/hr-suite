51549da46a0062bf376d2edc76252204584249f3
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-15 17:13:05 -0700

    Diseño navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/user">User</router-link>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img class="logo" src="@/assets/logo.png" alt="" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="link" to="/">Home</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -10,4 +31,18 @@ export default {};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.logo {
+  width: 40px;
+}
+.link {
+  margin: 1rem;
+}
+.navbar .navbar-collapse {
+  text-align: center;
+  color: var(--green);
+}
+.fa-solid {
+  font-size: 2rem;
+}
+</style>
