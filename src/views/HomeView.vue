f22b83a6fbcd12cb52b8349dcd168ffa67f8e135
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-15 18:33:32 -0700

    Visual design and CSS config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,181 @@
 <template>
-  <section class="Home">Home</section>
+  <section class="Home">
+    <h2 class="title">Manejo de Candidatos</h2>
+    <!-- Seccion input añadir un candidato -->
+    <div class="addSection">
+      <h4 class="addTitle">Añadir Candidato y fecha de entrevista</h4>
+      <form class="inputSection" @submit.prevent="addNewTodo">
+        <input
+          v-model="candidateName"
+          name="candidateName"
+          placeholder="Nombre del candidato"
+          autocomplete="off"
+          class="nameInput"
+        />
+        <input
+          class="dateInput"
+          type="date"
+          v-model="interviewDate"
+          name="interviewDate"
+        />
+        <button class="addCandidate">
+          <i class="fa-solid fa-plus"></i>
+        </button>
+      </form>
+    </div>
+    <!-- Seccion Lista candidatos -->
+    <div class="candidatesList">
+      <h4>Lista de Candidatos</h4>
+      <ul class="candidates">
+        <li class="candidate">
+          <div class="candidateInfo">
+            <div class="candidateImg">
+              <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="infoSection name">
+              <h6 class="subtitle">Nombre</h6>
+              <h4 class="sectionValue">José Robles</h4>
+            </div>
+            <div class="infoSection">
+              <h6 class="subtitle">Fecha Entrevista</h6>
+              <h5 class="sectionValue">06/05/2026</h5>
+            </div>
+            <div class="infoSection actions">
+              <router-link class="details" to="">Detalles</router-link>
+              <button class="deleteCandidate">
+                <i class="fa-solid fa-xmark"></i>
+              </button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {};
 </script>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: auto;
+}
+.title {
+  margin: 1rem 0;
+}
+/* Seccion añadir candidato */
+.addSection {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.inputSection {
+  background-color: var(--lightBlue);
+}
+input {
+  background-color: transparent;
+  border: none;
+  font-size: 1rem;
+  font-weight: 400;
+  appearance: none;
+  outline: none;
+}
+.nameInput {
+  margin: 0 1rem;
+  padding: 0 1rem;
+}
+.dateInput {
+  margin: 0 2rem;
+  padding: 0 1rem;
+  border-left: solid 1px black;
+}
+.addCandidate {
+  background-color: transparent;
+  border: none;
+  padding: 1rem;
+  transition: 0.4s;
+}
+.addCandidate:hover {
+  background-color: var(--yellow);
+}
+
+/* Seccion lista de candidatos */
+.candidatesList {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1rem;
+}
+.candidates {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  margin: 1rem 0;
+  padding: 0;
+}
+.candidate {
+  list-style: none;
+  box-shadow: 0 0 5px #00000040;
+}
+
+.candidateInfo {
+  display: flex;
+  align-items: center;
+}
+
+.candidateImg {
+  padding: 1rem;
+}
+.subtitle {
+  margin: 0;
+  color: grey;
+  font-size: 0.7rem;
+}
+.infoSection {
+  margin: 0 1rem 0 0;
+}
+.name {
+  width: 200px;
+}
+.sectionValue {
+  margin: 0;
+}
+.actions {
+  display: flex;
+  gap: 1rem;
+  margin: 0 0 0 1rem;
+  align-items: center;
+}
+.details {
+  text-decoration: none;
+  background-color: var(--blue);
+  color: var(--carbon);
+  padding: 0.3rem 0.7rem;
+  transition: 0.4s;
+  font-weight: 500;
+}
+.details:hover {
+  background-color: var(--grey);
+}
+.deleteCandidate {
+  border: none;
+  padding: 1rem;
+  transition: 0.4s;
+}
+
+.deleteCandidate:hover {
+  background-color: var(--red);
+}
+</style>
