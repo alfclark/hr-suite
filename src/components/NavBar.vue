1e63e76aaf413f50cca9dacb26565a15d46ec52d
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-17 22:41:53 -0700

    Code simplification

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,24 +4,6 @@
       <router-link class="navbar-brand" to="/">
         <img class="logo" src="@/assets/logo.png" alt="" />
       </router-link>
-      <!--  <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="link" to="/">Home</router-link>
-          </li>
-        </ul>
-      </div> -->
     </div>
   </nav>
 </template>
@@ -30,16 +12,8 @@
 export default {};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .logo {
   width: 40px;
 }
-/* .link {
-  margin: 1rem;
-}
-.navbar .navbar-collapse {
-  text-align: center;
-  color: var(--green);
-} */
 </style>
